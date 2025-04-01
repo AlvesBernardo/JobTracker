ccdf22f92bd687d4a51adf78c823372c746d9aa0
AlvesBernardo <bernardo.correia.alves@student.nhlstenden.com>
2025-04-01 13:03:56 +0200

    fix: cors

--- JobTracker/src/main/java/com/nhlstenden/configFiles/ConfigPlugin.java
+++ JobTracker/src/main/java/com/nhlstenden/configFiles/ConfigPlugin.java
@@ -1,6 +1,7 @@
 package com.nhlstenden.configFiles;
 
 import io.javalin.config.JavalinConfig;
+import io.javalin.plugin.bundled.CorsPluginConfig;
 
 import java.util.Properties;
 
@@ -10,28 +11,12 @@ public class ConfigPlugin
     private static final ClassLoader loader = Thread.currentThread().getContextClassLoader();
 
 
-//	InputStream input = loader.getResourceAsStream("/config.properites")
-//	{
-//		try (input == null)
-//		{
-//			if (input == null)
-//			{
-//				throw new RuntimeException("Configuration file not found in resources!");
-//			}
-//			properties.load(input);
-//		} catch (IOException e)
-//		{
-//			throw new RuntimeException("Failed to load configuration", e);
-//		}
-//	}
-
-
     public static void configure(JavalinConfig config)
     {
         config.bundledPlugins.enableCors(cors ->
         {
             String allowedOrigins = properties.getProperty("cors.allowedOrigins", "*");
-            cors.addRule(it -> it.anyHost());
+            cors.addRule(CorsPluginConfig.CorsRule::anyHost);
         });
     }
 }
--- JobTracker/src/main/java/com/nhlstenden/route/MainRoute.java
+++ JobTracker/src/main/java/com/nhlstenden/route/MainRoute.java
@@ -10,112 +10,78 @@ import io.javalin.Javalin;
 
 import java.io.InputStreamReader;
 import java.lang.reflect.Type;
-import java.util.Comparator;
 import java.util.function.Function;
 
-public class MainRoute<T>
-{
+public class MainRoute<T> {
+	private final JobApplicationService<T> jobApplicationService;
 	private SharedData<T> data;
 	private SearchRoutes<T> searchRoutes;
 	private SortAlgo<T> sortAlgo;
 	private BloomFilterMiddelware<T> bloomFilter;
-	private PriorityQueueRoute<T> priorityQueueRoute;
 	private boolean routesConfigured = false;
-import java.util.Map;
-import java.util.function.Function;
+	private PriorityQueueRoute<T> priorityQueueRoute;
+	private BayerMooreRoute<T> bayerMooreRoute;
 
-public class MainRoute<T> {
-        private SharedData<T> data;
-        private SearchRoutes<T> searchRoutes;
-        private SortAlgo<T> sortAlgo;
-        private BloomFilterMiddelware<T> bloomFilter;
-        private boolean routesConfigured = false;
-        private final JobApplicationService<T> jobApplicationService;
+	public MainRoute(JobApplicationService<T> jobApplicationService) {
+		this.jobApplicationService = jobApplicationService;
+	}
 
-        public MainRoute(JobApplicationService<T> jobApplicationService) {
-                this.jobApplicationService = jobApplicationService;
-        }
+	public void configureRoutes(Javalin app) {
+		app.post("/upload", ctx -> {
+			ctx.uploadedFiles("files").forEach(uploadedFile -> {
+				InputStreamReader reader = new InputStreamReader(uploadedFile.content());
+				try {
+					Type type = new TypeToken<MyArrayList<T>>() {}.getType();
+					MyArrayList<T> resultArray = JsonUtils.jsonFileToMapGson(reader, type);
 
-        public void configureRoutes(Javalin app) {
-                app.post("/upload", ctx -> {
-                        ctx.uploadedFiles("files").forEach(uploadedFile -> {
-                                InputStreamReader reader = new InputStreamReader(uploadedFile.content());
-                                try {
-                                        Type type = new TypeToken<MyArrayList<T>>() {}.getType();
-                                        MyArrayList<T> resultArray = JsonUtils.jsonFileToMapGson(reader, type);
+					if (this.data == null) {
+						this.data = new SharedData<>(resultArray);
+						this.bloomFilter = new BloomFilterMiddelware<>(resultArray.size(), new Function[]{Object::hashCode});
+						for (T element : resultArray) {
+							this.bloomFilter.add(element);
+						}
+					} else {
+						for (T element : resultArray) {
+							if (!this.bloomFilter.mightContain(element)) {
+								this.data.getSharedArray().add(element);
+								this.bloomFilter.add(element);
+							} else {
+								boolean isDuplicate = false;
+								for (T dataElement : this.data.getSharedArray()) {
+									if (dataElement.equals(element)) {
+										isDuplicate = true;
+										break;
+									}
+								}
+								if (!isDuplicate) {
+									this.data.getSharedArray().add(element);
+									this.bloomFilter.add(element);
+								}
+							}
+						}
+					}
 
-                                        if (this.data == null) {
-                                                this.data = new SharedData<>(resultArray);
-                                                this.bloomFilter = new BloomFilterMiddelware<>(resultArray.size(), new Function[]{Object::hashCode});
-                                                for (T element : resultArray) {
-                                                        this.bloomFilter.add(element);
-                                                }
-                                        } else {
-                                                for (T element : resultArray) {
-                                                        if (!this.bloomFilter.mightContain(element)) {
-                                                                this.data.getSharedArray().add(element);
-                                                                this.bloomFilter.add(element);
-                                                        } else {
-                                                                boolean isDuplicate = false;
-                                                                for (T dataElement : this.data.getSharedArray()) {
-                                                                        if (dataElement.equals(element)) {
-                                                                                isDuplicate = true;
-                                                                                break;
-                                                                        }
-                                                                }
-                                                                if (!isDuplicate) {
-                                                                        this.data.getSharedArray().add(element);
-                                                                        this.bloomFilter.add(element);
-                                                                }
-                                                        }
-                                                }
-                                        }
-
-
-                                        jobApplicationService.addApplications(this.data.getSharedArray());
-
-                                        ctx.status(200).result("Completed");
-
-                                        if (!routesConfigured) {
-                                                searchRoutes = new SearchRoutes<>(data);
-                                                searchRoutes.configureRoutes(app);
+					jobApplicationService.addApplications(this.data.getSharedArray());
 
 					ctx.status(200).result("Completed");
-					if (!routesConfigured)
-					{
+
+					if (!routesConfigured) {
 						searchRoutes = new SearchRoutes<>(data);
 						searchRoutes.configureRoutes(app);
 						sortAlgo = new SortAlgo<>(data);
 						sortAlgo.configureRoutes(app);
 						priorityQueueRoute = new PriorityQueueRoute<>(data);
 						priorityQueueRoute.configureRoutes(app);
+						bayerMooreRoute = new BayerMooreRoute<>(data);
+						bayerMooreRoute.configureRoutes(app);
+
 						this.routesConfigured = true;
 					}
-				} catch (Exception e)
-				{
+				} catch (Exception e) {
 					e.printStackTrace();
 					ctx.status(500).result("Missing file");
 				}
 			});
 		});
 	}
-                                                sortAlgo = new SortAlgo<>(data);
-                                                sortAlgo.configureRoutes(app);
-
-                                                BayerMooreRoute<T> bmRoute = new BayerMooreRoute<>(this.data);
-                                                bmRoute.configureRoutes(app);
-
-                                                this.routesConfigured = true;
-                                        }
-                                } catch (Exception e) {
-                                        e.printStackTrace();
-                                        ctx.status(500).result("Missing file");
-                                }
-                        });
-                });
-        }
-
-        public SharedData<T> getSharedData() {
-                return data;
-        }
 }
